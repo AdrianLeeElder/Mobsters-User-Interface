adaeeae40ad25908dfe3cb217424950c66c701d9
Adrian Elder <AdrianLeeElder@gmail.com>
2019-06-25 19:45:56 -0400

    Fix typo in vue.config env

--- vue.config.js
+++ vue.config.js
@@ -1,23 +1,23 @@
 var webpack = require('webpack');
 
 module.exports = {
-    // proxy all webpack dev-server requests starting with /api
-    // to our Spring Boot backend (localhost:8088) using http-proxy-middleware
-    // see https://cli.vuejs.org/config/#devserver-proxy
-    devServer: {
-      proxy: {
-        '/api': {
-          target: 'http://localhost:8050',
-          ws: true,
-          changeOrigin: true
-        }
+  // proxy all webpack dev-server requests starting with /api
+  // to our Spring Boot backend (localhost:8088) using http-proxy-middleware
+  // see https://cli.vuejs.org/config/#devserver-proxy
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8050',
+        ws: true,
+        changeOrigin: true
       }
-    },
-    configureWebpack: {
-      plugins: [
-        new webpack.EnvironmentPlugin([
-          'CLIENT_ID',
-        ]),
-      ]
     }
+  },
+  configureWebpack: {
+    plugins: [
+      new webpack.EnvironmentPlugin({
+        CLIENT_ID: '0oan3vml3k4TQUvJz356',
+      }),
+    ]
   }
+}
